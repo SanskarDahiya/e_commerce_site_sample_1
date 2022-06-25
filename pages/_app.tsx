d760cf091c0997153c3d214d70aeed24d77694f0
Sanskar <sanskar.dahiya@recro.io>
2022-06-25 12:01:41 +0530

    add footer & header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "twin.macro";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
