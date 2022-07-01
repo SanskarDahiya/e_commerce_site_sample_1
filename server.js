3a4d3a6433db5f46471aa8561415f5fda2654397
Sanskar <sanskar.dahiya@recro.io>
2022-07-01 19:13:51 +0530

    add admin

--- server.js
+++ server.js
@@ -3,39 +3,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const formidableMiddleware = require("express-formidable");
-const AdminBro = require("admin-bro");
-const AdminBroExpressjs = require("admin-bro-expressjs");
-// const next = require("next");
-const port = parseInt(process.env.PORT, 10) || 3001;
-const dev = process.env.NODE_ENV !== "production";
-// const app = next({ dev });
-
-// We have to tell AdminBro that we will manage mongoose resources with it
-AdminBro.registerAdapter(require("admin-bro-mongoose"));
-
-// express server definition
-const server = express();
-server.use(formidableMiddleware());
-
-// Resources definitions
-const User = mongoose.model("users", {
-  name: String,
-  email: String,
-  password: String,
-  _createdOn: Date,
-  _updatedOn: Date,
-  isAdmin: Boolean,
-});
-
-// Pass all configuration settings to AdminBro
-const adminBro = new AdminBro({
-  resources: [User],
-  rootPath: "/admin",
-});
-
-// Build and use a router which will handle all AdminBro routes
-const router = AdminBroExpressjs.buildRouter(adminBro);
-server.use(adminBro.options.rootPath, router);
+require("dotenv").config({ path: require("find-config")(".env") });
 
 // const handle = app.getRequestHandler();
 // server.all("*", (req, res) => {
@@ -48,10 +16,47 @@ const run = async () => {
     await mongoose.connect(process.env.MONGO_URI + process.env.MONGO_DB, {
       useNewUrlParser: true,
     });
+
+    const AdminBro = require("admin-bro");
+    const AdminBroExpressjs = require("admin-bro-expressjs");
+    // const next = require("next");
+    const port = parseInt(process.env.PORT, 10) || 3001;
+    // const dev = process.env.NODE_ENV !== "production";
+    // const app = next({ dev });
+
+    // We have to tell AdminBro that we will manage mongoose resources with it
+    AdminBro.registerAdapter(require("admin-bro-mongoose"));
+
+    // express server definition
+    const server = express();
+    server.use(formidableMiddleware());
+
+    // Resources definitions
+    const User = mongoose.model("users", {
+      name: String,
+      email: String,
+      password: String,
+      _createdOn: Date,
+      _updatedOn: Date,
+      isAdmin: Boolean,
+    });
+
+    // Pass all configuration settings to AdminBro
+    const adminBro = new AdminBro({
+      resources: [User],
+      rootPath: "/",
+    });
+
+    // Build and use a router which will handle all AdminBro routes
+    const router = AdminBroExpressjs.buildRouter(adminBro);
+    server.use(adminBro.options.rootPath, router);
+
+    await server.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  } else {
+    console.log(`Db Connection Failed`);
   }
-  await server.listen(port, () =>
-    console.log(`Example app listening on port 8080!`)
-  );
 };
 // app.prepare().then(async () => {
 run();
