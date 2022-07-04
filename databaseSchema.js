a3e5ecdaa863353a324ce65d5530ba0dc36c096d
Sanskar <sanskar.dahiya@recro.io>
2022-07-04 10:24:28 +0530

    remove cart schema from admin

--- databaseSchema.js
+++ databaseSchema.js
@@ -22,17 +22,18 @@ const PostSchema = new Schema({
   _updatedOn: Date,
 });
 
-const CartSchema = new Schema({
-  items: [{ type: Types.ObjectId, ref: "users" }],
-  _createdOn: Date,
-  _updatedOn: Date,
-});
+// const CartSchema = new Schema({
+//   items: [{ type: Types.ObjectId, ref: "users" }],
+//   itemsRef: { type: Map, of: String },
+//   _createdOn: Date,
+//   _updatedOn: Date,
+// });
 
 const Users = model("users", UserSchema);
 
 const Posts = model("items", PostSchema);
-const Carts = model("user_cart", CartSchema);
+// const Carts = model("user_cart", CartSchema);
 
-const DataBases = [Users, Posts, Carts];
+const DataBases = [Users, Posts];
 
 module.exports = DataBases;
