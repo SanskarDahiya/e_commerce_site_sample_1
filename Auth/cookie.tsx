9221348bdb01e66ac4b44df81507a3fff039e994
Sanskar <sanskar.dahiya@recro.io>
2022-07-08 12:02:04 +0530

    add auth

---
+++ Auth/cookie.tsx
@@ -0,0 +1,34 @@
+import Cookies from "js-cookie";
+import { NextApiRequest } from "next";
+import { verifyToken } from "./jwt";
+
+interface ObjectInterface<T> {
+  [key: string]: T;
+}
+
+export function getAccessTokenSSR(req: NextApiRequest): string {
+  let token;
+
+  if (req?.cookies) {
+    token = req.cookies.token;
+  } else if (req?.headers?.cookie) {
+    const parsedItems = {} as ObjectInterface<string>;
+    const cookiesItems = req.headers.cookie.split("; ");
+    cookiesItems.forEach((cookies: string) => {
+      const parsedItem = cookies.split("=");
+      parsedItems[parsedItem[0]] = decodeURI(parsedItem[1]);
+    });
+    token = parsedItems.token;
+  }
+  token = (token || "").split(" ").pop();
+  return token || "";
+}
+
+export const getUserFromCookie = () => {
+  let token = Cookies.get("token");
+  token = (token || "").split(" ").pop();
+  if (token) {
+    const profileData = verifyToken(token);
+    return profileData;
+  }
+};
---
+++ Auth/jwt.tsx
@@ -0,0 +1,53 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import {
+  AccessTokenInterface,
+  RefreshTokenInterface,
+  UserInterface,
+} from "@constants/Types";
+
+const SECRET_KEY = "MY CUSTOM KEY PASS PAIR";
+
+export function generateToken(
+  payload: string | Buffer | object,
+  options?: SignOptions
+): string {
+  return jwt.sign(payload, SECRET_KEY, options);
+}
+
+export function verifyToken(jwtToken: string, isLogError?: boolean) {
+  try {
+    if (jwtToken && jwtToken.startsWith("Bearer")) {
+      jwtToken = jwtToken.split(" ").pop() || "";
+    }
+    const result = jwt.verify(jwtToken, SECRET_KEY) as RefreshTokenInterface;
+    return { result };
+  } catch (e: any) {
+    isLogError && console.log("e:", e.message);
+    return { error: e };
+  }
+}
+
+export const generateAccessToken = (user: UserInterface) => {
+  const options = { expiresIn: "1d" };
+  const accessTokenPayload: AccessTokenInterface = {
+    _id: user._id.toString(),
+    isAdmin: user.isAdmin,
+    email: user.email,
+    _createdOn: new Date(),
+    _updatedOn: user._updatedOn,
+  };
+  return generateToken(accessTokenPayload, options);
+};
+
+export const generateRefreshToken = (user: UserInterface) => {
+  const options = { expiresIn: "5m" };
+  const accessTokenPayload: RefreshTokenInterface = {
+    _id: user._id.toString(),
+    isAdmin: user.isAdmin,
+    email: user.email,
+    _createdOn: new Date(),
+    _updatedOn: user._updatedOn,
+    isRefreshToken: true,
+  };
+  return generateToken(accessTokenPayload, options);
+};
