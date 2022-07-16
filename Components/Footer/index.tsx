cbb8b4503acca7c541ab1b5d8d781a3ab6a6c359
Sanskar <sanskar.dahiya@recro.io>
2022-07-16 15:46:47 +0530

    version upgrade  | deploy

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { version } from "package.json";
 function Footer() {
   return (
-    <footer className="container mx-auto bg-white py-8 border-t border-gray-400">
+    <footer className="relative container mx-auto bg-white py-8 border-t border-gray-400">
+      <div className="absolute bottom-2	text-sm antialiased font-light tracking-widest">
+        Version: {version}
+      </div>
       <div className="container flex px-3 py-8 ">
         <div className="w-full mx-auto flex flex-wrap">
           <div className="flex w-full lg:w-1/2 ">
